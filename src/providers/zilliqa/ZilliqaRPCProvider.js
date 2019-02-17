47be34293af7989b1893aa0829794acf6ec528ee
Matthew Black <mblackmblackmblack@gmail.com>
2019-02-17 00:38:01 -0700

    Working secret generation zilliqa

--- src/providers/zilliqa/ZilliqaRPCProvider.js
+++ src/providers/zilliqa/ZilliqaRPCProvider.js
@@ -1,13 +1,13 @@
 import _ from 'lodash'
 import BigNumber from 'bignumber.js'
-import JsonRpcProvider from '../JsonRpcProvider'
+import Provider from '../../Provider'
 
 const { Transaction } = require('@zilliqa-js/account');
 const { BN, Long, bytes, units } = require('@zilliqa-js/util');
 const { Zilliqa } = require('@zilliqa-js/zilliqa');
 const CP = require ('@zilliqa-js/crypto');
 
-export default class ZilliqaRPCProvider {
+export default class ZilliqaRPCProvider extends Provider {
   constructor (uri, refundPrivKey, redeemPrivKey) {
     super(uri, refundPrivKey, redeemPrivKey)
     this._zilliqa = new Zilliqa(uri)
@@ -16,13 +16,14 @@ export default class ZilliqaRPCProvider {
   }
 
   async getAddresses () {
-    const refundAddress = CP.getAddressFromPrivateKey(refundPrivKey)
-    const redeemAddress = CP.getAddressFromPrivateKey(redeemPrivKey)
+    const refundAddress = CP.getAddressFromPrivateKey(this._refundPrivKey)
+    const redeemAddress = CP.getAddressFromPrivateKey(this._redeemPrivKey)
     return [{ address: refundAddress }, { address: redeemAddress}]
   }
 
   async signMessage (message) {
     const refundPubKey = CP.getPubKeyFromPrivateKey(this._refundPrivKey)
-    return CP.sign(message, this._refundPrivKey, refundPubKey)
+    const msgBuffer = Buffer.from(message)
+    return CP.sign(msgBuffer, this._refundPrivKey, refundPubKey)
   }
 }
