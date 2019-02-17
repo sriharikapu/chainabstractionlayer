2408b875b498db79bc5f14b117795a9c933739b2
Matthew Black <mblackmblackmblack@gmail.com>
2019-02-17 00:11:21 -0700

    Add zilliqa-rpc-provider

--- src/providers/zilliqa/ZilliqaRPCProvider.js
+++ src/providers/zilliqa/ZilliqaRPCProvider.js
@@ -1,1 +1,28 @@
-zilliqa rpc provider
+import _ from 'lodash'
+import BigNumber from 'bignumber.js'
+import JsonRpcProvider from '../JsonRpcProvider'
+
+const { Transaction } = require('@zilliqa-js/account');
+const { BN, Long, bytes, units } = require('@zilliqa-js/util');
+const { Zilliqa } = require('@zilliqa-js/zilliqa');
+const CP = require ('@zilliqa-js/crypto');
+
+export default class ZilliqaRPCProvider {
+  constructor (uri, refundPrivKey, redeemPrivKey) {
+    super(uri, refundPrivKey, redeemPrivKey)
+    this._zilliqa = new Zilliqa(uri)
+    this._refundPrivKey = refundPrivKey
+    this._redeemPrivKey = redeemPrivKey
+  }
+
+  async getAddresses () {
+    const refundAddress = CP.getAddressFromPrivateKey(refundPrivKey)
+    const redeemAddress = CP.getAddressFromPrivateKey(redeemPrivKey)
+    return [{ address: refundAddress }, { address: redeemAddress}]
+  }
+
+  async signMessage (message) {
+    const refundPubKey = CP.getPubKeyFromPrivateKey(this._refundPrivKey)
+    return CP.sign(message, this._refundPrivKey, refundPubKey)
+  }
+}
